60c000674a60dd92c85b8c100303ae4ab59d59a9
Stanley Lee <sachih@live.com>
2019-11-12 23:47:35 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -74,3 +74,13 @@ def reduce_to_all_true(array)
   end
     return true
 end
+
+
+def reduce_to_any_true(array)
+  i = 0
+  while i<array.length
+    return true if array[i]
+    i+= 1
+  end
+    return false
+end
