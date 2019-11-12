78d0a48d083bca387eb5dceff98c9595891a082b
Stanley Lee <sachih@live.com>
2019-11-12 23:40:21 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -47,7 +47,7 @@ end
 def reduce_to_total(array, starting_point = 0)
 i = 0
 total = 0
-if starting_point > 0 do
+if starting_point > 0 
   total = starting_point
   while i < array.length do
     total += array[i]
